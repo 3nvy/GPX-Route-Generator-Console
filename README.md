51b103ed957dc514502ed6fddaa6b361f4118c67
3nvy <renatompestana@gmail.com>
2020-04-25 20:03:32 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # GPX Route Generator
 
+This app generates a GPX file with coordinates sorted in an optimal path, based on the nearest-neighbor algorithm. By providing a file with multiple coordinates to the app, it outputs a GPX file with a optimal path with the first coordinate as a starting point.
+
+The app comes with an example file (coords.txt) which will output an optimal path GTX file for Central Park, New York.
+
+# How to use
+
+Install necesary dependencies
+<pre>
+npm i
+</pre>
+
+Run app
+<pre>
+node generator in=coords.txt out=central_park
+</pre>
+
+Argument | Description
+------------ | -------------
+in | name & extension of file to read the coordinates from
+out | desired name for te exported GPX file
+
+* All arguments are optional
+
+# Output
+
+The outputed file can now be imported in any app capable of tracing routes from a GPX file.
---
+++ generate.js
@@ -0,0 +1,62 @@
+var sphereKnn = require("sphere-knn");
+var fs = require("fs");
+
+const args = process.argv.slice(2).reduce((acc, arg) => {
+  const [k, v] = arg.split("=");
+  acc[k] = v;
+  return acc;
+}, {});
+
+const saveToFile = (data) => {
+  fs.writeFile(`${args["out"] || "default_route"}.gpx`, data, (err) => {
+    // throws an error, you could also catch it here
+    if (err) throw err;
+    // success case, the file was saved
+    console.log("Route saved!");
+  });
+};
+
+const generateGPX = (
+  sortedArray
+) => `<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<gpx version="1.1" xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd">
+${sortedArray
+  .map((p) => {
+    const [lat, lon] = p.split(",");
+    return `<wpt lat="${lat}" lon="${lon}"></wpt>`;
+  })
+  .join("\n")}
+</gpx>`;
+
+try {
+  const contents = fs.readFileSync(`${args["in"] || "coords.txt"}`, "utf8");
+
+  const posArray = contents.split("\r\n").map((coords) => {
+    const [lat, lon] = coords.split(",");
+    return [lat, lon];
+  });
+
+  const firstPos = posArray.shift();
+  let sortedArray = [firstPos.toString()];
+  let startPoint = firstPos;
+
+  while (posArray.length > 0) {
+    var point = sphereKnn(posArray)(startPoint[0], startPoint[1], 1, 999999);
+    startPoint = point[0];
+    sortedArray.push(startPoint.toString());
+
+    var index = posArray.reduce(
+      (acc, el, i) => (el.toString() === startPoint.toString() ? acc + i : acc),
+      0
+    );
+    posArray.splice(index, 1);
+  }
+
+  saveToFile(generateGPX(sortedArray));
+} catch (err) {
+  console.error(
+    `File '${
+      args["in"] || "coords.txt"
+    }' doesnt exists or content is in wrong format!`
+  );
+}
